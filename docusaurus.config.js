00b9425a9600db4a1b0985a97329a4b3e2ec344c
powerfeatherdev <dev@powerfeather.dev>
2023-04-29 20:51:14 +0900

    Basic edits to site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'The Ultimate LiPo-Powered ESP32-S3 Dev Board',
-  tagline: 'Coming Soon.',
+  title: 'ESP32-S3 PowerFeather',
+  tagline: 'The Ultimate LiPo-Powered ESP32-S3 Dev Board',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -70,7 +70,64 @@ const config = {
           src: 'img/powerfeather.svg',
         },
         items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/powerfeather',
+            label: 'GitHub',
+            position: 'right',
+          },
         ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/powerfeather',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
