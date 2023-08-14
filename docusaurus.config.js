64c9a327e6312798bab5908b5724e6943fb864a8
powerfeatherdev <dev@powerfeather.dev>
2023-08-15 07:27:36 +0900

    Change tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'PowerFeather',
-  tagline: 'Ultimate LiPo-Powered Boards',
+  tagline: 'Ultimate LiPo-Powered Development Boards',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
