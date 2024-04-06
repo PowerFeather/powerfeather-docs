65f77579a445f47ec4d8f7b6a8bbaa66a22fdcd3
powerfeatherdev <dev@powerfeather.dev>
2024-04-06 19:59:06 +0800

    Change edit branch url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/PowerFeather/powerfeather-docs/edit/master/',
+            'https://github.com/PowerFeather/powerfeather-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
