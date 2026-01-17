355b24117c7ca3dcfb1bd9294ed9e55ee86df6ba
powerfeatherdev <dev@powerfeather.dev>
2026-01-17 15:57:38 +0900

    Fix build warnings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,11 @@ const config = {
   baseUrl: '/',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
